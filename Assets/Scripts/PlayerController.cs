90bb48519a1a3570545a2f6700b3e5e1d281971c
João Marcelo Silva <jmporto@outlook.com>
2024-10-03 06:11:32 -0300

    [Feature]: Lesson 2.2 - Food Flight
    1- Make the projectile fly forwards
    2- Make the projectile into a prefab
    3- Test for spacebar press
    4- Launch projectile on spacebar press
    5- Make animals into prefabs
    6- Destroy projectiles offscreen
    7- Destroy animals offscreen

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,9 +7,14 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10.0f;
+    public GameObject projectilePrefab;
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
         if (transform.position.x < -xRange)
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
